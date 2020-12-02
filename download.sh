f8bbbad4da1b86783f0901d8d420b18a86b554f6
kimown <kimown@163.com>
2020-12-02 16:49:30 +0800

    chore

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #https://developer.nvidia.com/cuda-11.0-update1-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=runfilelocal
-wget https://us.download.nvidia.com/tesla/418.116.00/NVIDIA-Linux-x86_64-450.80.02.run
+wget https://us.download.nvidia.com/tesla/450.80.02/NVIDIA-Linux-x86_64-450.80.02.run
 wget https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda_11.0.3_450.51.06_linux.run
 wget https://www.ffmpeg.org/releases/ffmpeg-4.3.1.tar.bz2
 
