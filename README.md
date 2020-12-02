26df90e97236037b7b55a1a6de9b459740d5c3cc
kimown <kimown@163.com>
2020-12-02 17:40:08 +0800

    chore

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
 # run
 ```shell
-# NVIDIA-SMI 450.80.02    Driver Version: 450.80.02    CUDA Version: 11.0
 sh download.sh
 
-md5sum *.*
+md5sum NVIDIA-Linux-x86_64-450.80.02.run cuda_11.0.3_450.51.06_linux.run ffmpeg-4.3.1.tar.bz2
+c68b3500fb5ceb17a0fcebcbb143dad8  NVIDIA-Linux-x86_64-450.80.02.run
+70af4cebe30549b9995fb9c57d538214  cuda_11.0.3_450.51.06_linux.run
+804707549590e90880e8ecd4e5244fd8  ffmpeg-4.3.1.tar.bz2
+
+# https://www.yuque.com/kimown/hiy9e0/hws5kp
+nvidia-installer --uninstall
+apt-get purge nvidia. 
+update-initramfs -u
+
+chmod +x NVIDIA-Linux-x86_64-450.80.02.run 
+./NVIDIA-Linux-x86_64-450.80.02.run
+nvidia-smi 
+Wed Dec  2 17:11:03 2020       
++-----------------------------------------------------------------------------+
+| NVIDIA-SMI 450.80.02    Driver Version: 450.80.02    CUDA Version: 11.0     |
+|-------------------------------+----------------------+----------------------+
+
+# 安装kit:https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#setting-up-nvidia-container-toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+echo $distribution
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+echo $?
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
+# 重置/etc/docker/daemon.json： https://www.yuque.com/kimown/hiy9e0/ogbaeb
+ 
+docker run --rm --gpus all debian:10 nvidia-smi
 
 
 docker build -t docker_debian10_cuda11_nvenc10:latest .
--- build.sh
+++ build.sh
@@ -17,84 +17,98 @@ EOT
 
 apt-get update && apt-get -y install build-essential vim wget git kmod libxml2-dev sudo
 
+
 chmod +x cuda_11.0.3_450.51.06_linux.run
+apt-get update && apt-get -y install libxml2-dev
 ./cuda_11.0.3_450.51.06_linux.run --no-opengl-libs --toolkit --samples --silent --override
 rm -rf cuda_11.0.3_450.51.06_linux.run
-#echo "export PATH=/usr/local/cuda-11.1/bin:\$PATH" >>~/.bashrc
-#echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:\$LD_LIBRARY_PATH" >>~/.bashrc
-#export PATH=/usr/local/cuda-10.1/bin:$PATH
-#export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH
-#source ~/.bashrc
-#find / -name nvcc
-#nvcc  --version
-#
-#
-#cd /docker_debian10_cuda11_nvenc10
-#cd nv-codec-headers
-#git checkout sdk/10.0
-#make
-#make install
-#cd ..
-#rm -rf nv-codec-headers
-#
-#cd /docker_debian10_cuda11_nvenc10
-#sudo apt-get update -qq && sudo apt-get -y install \
-#  autoconf \
-#  automake \
-#  build-essential \
-#  cmake \
-#  git-core \
-#  libass-dev \
-#  libfreetype6-dev \
-#  libgnutls28-dev \
-#  libsdl2-dev \
-#  libtool \
-#  libva-dev \
-#  libvdpau-dev \
-#  libvorbis-dev \
-#  libxcb1-dev \
-#  libxcb-shm0-dev \
-#  libxcb-xfixes0-dev \
-#  pkg-config \
-#  texinfo \
-#  wget \
-#  yasm \
-#  zlib1g-dev
-#  
-#sudo apt-get install nasm libx264-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
-#sudo apt-get install libunistring-dev libgnutls28-dev
-#
-#export DIR=`pwd`
-#mkdir -p ffmpeg_sources ffmpeg_build bin
-#cd $DIR/ffmpeg_sources
-#tar xjvf ffmpeg-4.3.1.tar.bz2
-#cd ffmpeg-4.3.1 
-#PATH="$DIR/bin:$PATH" PKG_CONFIG_PATH="$DIR/ffmpeg_build/lib/pkgconfig" ./configure \
-#  --prefix="$DIR/ffmpeg_build" \
-#  --pkg-config-flags="--static" \
-#  --extra-cflags="-I$DIR/ffmpeg_build/include" \
-#  --extra-ldflags="-L$DIR/ffmpeg_build/lib" \
-#  --extra-libs="-lpthread -lm" \
-#  --bindir="$DIR/bin" \
-#  --enable-static \
-#  --disable-shared \
-#  --enable-gpl \
-#  --enable-gnutls \
-#  --disable-libaom \
-#  --enable-libass \
-#  --enable-libfdk-aac \
-#  --enable-libfreetype \
-#  --enable-libmp3lame \
-#  --enable-libopus \
-#  --enable-libvorbis \
-#  --enable-libvpx \
-#  --enable-libx264 \
-#  --disable-libx265 \
-#  --enable-nonfree \
-#  --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 \
-#  --enable-cuda --enable-cuvid --enable-nvenc
-#  
-#PATH="$PWD/bin:$PATH" make -j$(nproc)
-#make install
-#cd ../..
-#rm -rf ffmpeg_sources
+echo "export PATH=/usr/local/cuda-11.0/bin:\$PATH" >>~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64:\$LD_LIBRARY_PATH" >>~/.bashrc
+export PATH=/usr/local/cuda-11.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64:$LD_LIBRARY_PATH
+source ~/.bashrc
+find / -name nvcc
+nvcc  --version
+
+
+cd /docker_debian10_cuda11_nvenc10
+cd nv-codec-headers
+git checkout sdk/10.0
+make
+make install
+cd ..
+rm -rf nv-codec-headers
+
+cd /docker_debian10_cuda11_nvenc10
+sudo apt-get update -qq && sudo apt-get -y install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  git-core \
+  libass-dev \
+  libfreetype6-dev \
+  libgnutls28-dev \
+  libsdl2-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  pkg-config \
+  texinfo \
+  wget \
+  yasm \
+  zlib1g-dev
+
+sudo apt-get install nasm libx264-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
+sudo apt-get install libunistring-dev libgnutls28-dev
+
+export DIR=`pwd`
+mkdir -p ffmpeg_sources ffmpeg_build bin
+cd $DIR/ffmpeg_sources
+tar xjvf ffmpeg-4.3.1.tar.bz2
+cd ffmpeg-4.3.1 
+PATH="$DIR/bin:$PATH" PKG_CONFIG_PATH="$DIR/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$DIR/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$DIR/ffmpeg_build/include" \
+  --extra-ldflags="-L$DIR/ffmpeg_build/lib" \
+  --extra-libs="-lpthread -lm" \
+  --bindir="$DIR/bin" \
+  --enable-static \
+  --disable-shared \
+  --enable-gpl \
+  --enable-gnutls \
+  --disable-libaom \
+  --enable-libass \
+  --enable-libfdk-aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libopus \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --disable-libx265 \
+  --enable-nonfree \
+  --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 \
+  --enable-cuda --enable-cuvid --enable-nvenc
+
+PATH="$PWD/bin:$PATH" make -j$(nproc)
+make install
+cd ..
+cd bin
+
+./ffmpeg -codecs|grep nvenc
+./ffmpeg -y -f lavfi -i color=c=white@1:duration=500:s=qcif:r=10:size=1280x720 input.mp4
+./ffmpeg -i input.mp4  frame_%d.jpeg
+./ffmpeg -i input.mp4 -c:v h264_nvenc output.mp4
+
+echo 'export PATH="/docker_debian10_cuda11_nvenc10/ffmpeg-4.3.1-amd64-static/bin:$PATH"' >> /etc/profile
+echo 'cat /etc/profile'
+cat /etc/profile
+echo 'export PATH="/docker_debian10_cuda11_nvenc10/ffmpeg-4.3.1-amd64-static/bin:$PATH"' >> ~/.bashrc
+echo 'cat ~/.bashrc'
+cat ~/.bashrc
